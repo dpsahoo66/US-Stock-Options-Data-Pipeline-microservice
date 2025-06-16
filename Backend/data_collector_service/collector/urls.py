8d99b869fc9bf3c365d9786fb4e93c08155af655
HARSHDEEP MAHAJAN <65184650+hdm08@users.noreply.github.com>
2025-06-16 15:45:15 +0100

    Data collector setup

---
+++ Backend/data_collector_service/collector/urls.py
@@ -0,0 +1,9 @@
+# data-collector/collector/urls.py
+from django.urls import path
+from collector import views
+
+urlpatterns = [
+    path('fetch_each_day_data/', views.fetch_each_day_data, name='fetch_each_day_data'),
+    path('fetch_last_5min_data/', views.fetch_last_5min_data, name='fetch_last_5min_data'),
+    path('fetch_option_data/', views.fetch_option_data, name='fetch_option_data'),
+]
